cdfe9f6bd3acc64862185608ae56f9ab2c6e69ae
gary boland <garyboland@gmail.com>
2014-05-28 14:47:16 +0100

    think python 18.1 & 17.7

---
+++ 17.7.py
@@ -0,0 +1,9 @@
+class Kangeroo(object):
+  def __init__(self, contents = []):
+    self.pouch_contents = contents
+    
+  def put_in_pouch(self, newitem):
+    self_contents.append(newitem)
+    
+  def __str__(self):
+    print  
